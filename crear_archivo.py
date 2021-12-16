299b24068590bd6ab7fa184567c538cab005834e
juan camilo <juanca2.0@hotmail.com>
2021-12-15 22:05:36 -0500

    agregando texto en una sola linea

--- crear_archivo.py
+++ crear_archivo.py
@@ -5,17 +5,17 @@ def name_file():
     # convertir nombres en listas
     with open("./names_file.txt", "r", encoding="utf-8") as file:
         for line in file:
-            # .rstrip('\n') sirve para elimunar saltos de linea 
+            # .rstrip('\n') sirve para eliminar saltos de linea 
             names.append(line.rstrip('\n'))
     # recorrer lista y generaar archivo 
     for line in  names:
         file =  open(line, "w", encoding="utf-8")
-        file.write("# language: es\n") 
-        file.write("@Cucumber\n") 
-        file.write("  Escenario:\n")
-        file.write("    Dado El usuario ingresa a carga de trayectos\n")
-        file.write("    Cuando El usuario va a cargar un archivo de Excel\n")
-        file.write("    Entonces El archivo se carga al sistema\n")
+        file.write("# language: es\n@Cucumber\n  Escenario:\n    Dado El usuario ingresa a carga de trayectos\n    Cuando El usuario va a cargar un archivo de Excel\n    Entonces El archivo se carga al sistema\n") 
+        # file.write("@Cucumber\n") 
+        # file.write("  Escenario:\n")
+        # file.write("    Dado El usuario ingresa a carga de trayectos\n")
+        # file.write("    Cuando El usuario va a cargar un archivo de Excel\n")
+        # file.write("    Entonces El archivo se carga al sistema\n")
     
 
 
